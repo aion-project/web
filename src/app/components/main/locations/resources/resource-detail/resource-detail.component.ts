864249b5eaa0a7cd2012a43bc2203feb576d3ab5
ShanManage <shan.96rt@gmail.com>
2019-10-22 11:25:29 +0530

    Intergrated resorces details page with backend

--- src/app/components/main/locations/resources/resource-detail/resource-detail.component.ts
+++ src/app/components/main/locations/resources/resource-detail/resource-detail.component.ts
@@ -1,4 +1,12 @@
+import { ConfirmDialogComponent } from './../../../../common/confirm-dialog/confirm-dialog.component';
+import { MatDialog } from '@angular/material';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ResourceService } from './../../../../../services/resource.service';
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { first } from 'rxjs/operators';
+import { ResourceCreateEditComponent } from '../resource-create-edit/resource-create-edit.component';
+import { Resource } from 'src/app/model/Resource';
 
 @Component({
   selector: 'app-resource-detail',
@@ -7,9 +15,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResourceDetailComponent implements OnInit {
 
-  constructor() { }
+  private resourceId: String
+
+  resource: Resource
+  isAdmin: boolean = false
+
+  constructor(
+    private userService: UserService,
+    private resourceService: ResourceService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private dialog: MatDialog,
+  ) { }
 
   ngOnInit() {
+    this.userService.isRole("admin").pipe(first()).subscribe((isAdmin: boolean) => {
+      this.isAdmin = isAdmin
+    })
+    this.activatedRoute.paramMap.pipe(first()).subscribe((map) => {
+      this.resourceId = map.get("resourceId")
+      this.fetchResourceInfo()
+    })
+  }
+
+  onEditResource() {
+    const dialogRef = this.dialog.open(ResourceCreateEditComponent, {
+      width: '640px',
+      data: this.resourceId
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.fetchResourceInfo();
+      }
+    });
+  }
+
+  // onDelete() {
+  //   const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+  //     width: '320px'
+  //   });
+
+  //   dialogRef.afterClosed().subscribe(result => {
+  //     if (result) {
+  //       this.resourceService.delete(this.resourceId).toPromise().then(_ => {
+  //         this.router.navigateByUrl("/locations/resources")
+  //       })
+  //     }
+  //   });
+  // }
+
+  fetchResourceInfo() {
+    this.resourceService.get(this.resourceId).pipe(first()).subscribe(resource => {
+      this.resource = resource
+    })
   }
 
 }
