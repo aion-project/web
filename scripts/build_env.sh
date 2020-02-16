d0e41f11367a569b6d2281c28d1265175aa26522
Udesh Kumarasinghe <mail@udesh.xyz>
2020-02-16 21:47:09 +0530

    Configured base urls

---
+++ scripts/build_env.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "Coping"
+rm -rf src/app/config/app-config.ts
+echo "export const AppConfig = { BASE_URL: 'https://aion-server.herokuapp.com', OKTA_SIGN_IN_WIDGET_CONFIG: { baseUrl: 'https://dev-107152.okta.com', clientId: '0oa1hsio8iqqxWKTd357', redirectUri: '${BASE_URL}/implicit/callback', authParams: { issuer: 'default', pkce: true }}, OKTA_CLIENT_CONFIG: { issuer: 'https://dev-107152.okta.com/oauth2/default', clientId: '0oa1hsio8iqqxWKTd357', redirectUri: '${BASE_URL}/implicit/callback', storage: 'localStorage' }" >> src/app/config/app-config.ts
+echo "Done"
