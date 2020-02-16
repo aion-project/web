516900fee7c6386409c72ff29061c46e0c2cb4e5
Udesh Kumarasinghe <mail@udesh.xyz>
2020-02-16 09:29:13 +0530

    Made responsive

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
@@ -6,23 +6,32 @@ import { tap, first, filter } from 'rxjs/operators';
 import { AppConfig } from 'src/app/config/app-config';
 import { OktaAuthService } from '@okta/okta-angular';
 import { User } from 'src/app/model/User';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements OnInit, OnDestroy {
 
+  private _mobileQueryListener: () => void;
+
+  mobileQuery: MediaQueryList;
   user: User;
 
   constructor(
     private userService: UserService,
     private oktaService: OktaAuthService,
-    private router: Router
+    private router: Router,
+    private changeDetectorRef: ChangeDetectorRef,
+    private media: MediaMatcher
   ) { }
 
   ngOnInit() {
+    this.mobileQuery = this.media.matchMedia('(max-width: 1024px)');
+    this._mobileQueryListener = () => this.changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
     this.userService.me(true).pipe(
       filter(user => user != null),
     ).subscribe(res => {
@@ -35,6 +44,10 @@ export class MainComponent implements OnInit {
         this.router.navigate(['/activate']);
       }
     });
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
   }
 
   logout() {
